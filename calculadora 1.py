7de3663ca5f71357f0df700528be7be049e4c763
FedeLibarona <flibarona@gmail.com>
2022-11-14 21:12:45 -0300

    calculadora 1.py
    calculadora 1.py andando

--- calculadora 1.py
+++ calculadora 1.py
@@ -1,27 +1,78 @@
-number1=int(input("ingresa un numero:"))
-number2=int(input("ingresar otro numero:"))
+number1=float(input("ingresa un numero:"))
+number2=float(input("ingresar otro numero:"))
 
 eleccion = 0
 
-while eleccion != 4:
+while eleccion != 8:
     print("""
     Indique la opearacion:
     
     1) suma
-    2) resta 
-    3) cambio de valores 
-    4) salir
+    2) resta
+    3) multiplicacion
+    4) divicion
+    5) cambio de valores 
+    6) seguir operando
+    7) salir
     """)  
 
     eleccion = int(input())
     
-    if eleccion -- 1:
+    if eleccion == 1:
         print(" ")
     
-        print("resultado: ", number1,"+" , number2,"-",number1+number2)
+        print("resultado: ", number1,"+" , number2,"=",number1+number2)
+        numeroanterior = number1+number2
         
-    if eleccion -- 2:
+    if eleccion == 2:
         print(" ")
-        print("resultado: ",number1,"-", number2,"-",number1-number2)
+        print("resultado: ",number1,"-", number2,"=",number1-number2)
+        numeroanterior = number1-number2
+    
+    if eleccion == 3:
+        print(" ")
+        print("resultado: ", number1,"*", number2,"=",number1*number2)
+        numeroanterior = number1*number2
+        
+    if eleccion == 4:
+        print(" ")
+        print("resultado: ", number1,"/", number2,"=",number1/number2)
+        numeroanterior = number1/number2
+        
+        
+    if eleccion == 5:
+        
+       number1 = float(input("cambio de valores"))
+       number2 = float(input("cambio de valores"))  
+    
+    if eleccion == 6:
+        
+        number1 = numeroanterior
+        number2 = float(input("ingresar otro numero"))     
+        
+    if eleccion == 7:
+        
+         print ("gracias por usar esta calculadora") 
+     
+        
+        
+      
+        
+         
+        
+        
+        
+                
+        
+
+    
+    
+    
+        
+        
+    
+        
+         
+           
         
     
